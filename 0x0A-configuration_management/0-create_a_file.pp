582b5c67db8ab3608d4e36bb5cb9cfee06b4f3e1
root <root@Mouadh.localdomain>
2022-01-03 16:38:20 +0100

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file { '/tmp/school': #the path of the new file
+node default {
+  file { '/tmp/school': #the path of the new file
     ensure => 'present',
     content => 'I love Puppet', #this text will be inside the file
     owner => 'www-data',
     group => 'www-data',
     mode => '0744',
   }
+}
